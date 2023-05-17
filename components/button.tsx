7ec3d718a19bf726f7c793e42ff48f7727204512
Edgar Dimas <edgar.dimas.ir@gmail.com>
2023-05-17 16:37:09 +0700

    second commit

--- components/button.tsx
+++ components/button.tsx
@@ -4,6 +4,21 @@ React apps are made out of components. A component is a piece of UI that has its
 appearance. A component can be as small as button, or as large as an entire page. 
 */
 
-export default function MyButton() {
-  return <button>I'm a button</button>;
+export default function MyButton({ count, onClick }: any) {
+  return <button onClick={onClick}> Clicked {count} times</button>;
 }
+
+/**
+ * Notice that <MyButton /> starts with a capital letter. That's how you know it's a React component.
+ * React component names must always start with a capital letter, while HTML tags must be lowercase.
+ */
+
+/**
+ * The export default keywords specify the main component in the file. If you're not
+ * familiar with some piece of JavaScript syntax, MDN and javascript.info have a great references.
+ */
+
+/**
+ * The markup syntax you've seen above is called JSX. it is optional, but most React projcest use JSX
+ * for its conveinence.  JSX is stricter than HRML. you have to c
+ */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 import Image from "next/image";
 import MyButton from "../components/button";
 import Profile from "../components/user";
+import ShoppingList from "../components/shppping";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <>
       <h1>Hello</h1>
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <h1></h1>
+      <MyButton count={count} onClick={handleClick} />
       <Profile />
+      <ShoppingList />
     </>
   );
 }
