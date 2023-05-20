bc0feba5cfc5d216f98cad10266714f61cdeadb0
Edgar Dimas <edgar.dimas.ir@gmail.com>
2023-05-20 20:40:14 +0700

    third commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createRoot } from "react-dom/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
